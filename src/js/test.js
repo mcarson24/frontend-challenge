5d90a8b46ec13fb522c011521ffb2dfd9c468b67
Matthew Carson <matthew24carson@gmail.com>
2019-11-10 23:10:59 -0500

    Map five closest stations to a user supplies address

--- src/js/test.js
+++ src/js/test.js
@@ -8,23 +8,12 @@ import Popup from 'ol-popup';
 import HTMLRenderer from './HTMLRenderer'
 import Station from './Station'
 import {center, boundaries} from './Philadelphia'
-import debounce from 'lodash'
 
 const haversine = require('haversine-distance')
-
 const addressInput = document.querySelector('#address')
-addressInput.addEventListener('keydown', ({key, target}) => {
-  if (key == 'Enter') {
-    const address = target.value
-  }
-})
+let stations = []
+let markers = []
 
-// Ordering by closest to City Hall:
-// const orderedStations = results.features.sort((a, b) => {
-//   return haversine({ lat: 39.95238, lon: -75.16267 }, { lat: a.geometry.coordinates[1], lon: a.geometry.coordinates[0]}) > 
-//          haversine({ lat: 39.95238, lon: -75.16267 }, { lat: b.geometry.coordinates[1], lon: b.geometry.coordinates[0]})
-// })
-// console.log(orderedStations)
 
 document.addEventListener('DOMContentLoaded', () => {
   const map = new Map({
@@ -40,6 +29,15 @@ document.addEventListener('DOMContentLoaded', () => {
       extent: boundaries
     })
   });
+  const userMarker = new Overlay({
+    position: '',
+    positioning: 'top-center',
+    element: document.createElement('div'),
+    stopEvent: false,
+    className: `marker input`
+  })
+  
+
 
   const renderer = new HTMLRenderer(map)
   const sidebar = document.getElementById('sidebar')
@@ -47,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('https://dkw6qugbfeznv.cloudfront.net/')
     .then(response => response.json())
     .then(results => {
+      stations = results.features
       results.features.forEach(location => {
         const station = new Station(location)
         sidebar.appendChild(renderer.createStationInfoDiv(station, {
@@ -54,15 +53,60 @@ document.addEventListener('DOMContentLoaded', () => {
           title: ['text-xl', 'font-bold'],
           paragraph: ['text-base']
         }))
-        map.addOverlay(new Overlay({
+        const newMarker = new Overlay({
           position: fromLonLat([station.coordinates.longitude, station.coordinates.latitude]),
           positioning: 'top-left',
           element: document.createElement('div'),
           stopEvent: false,
           className: `marker ${station.status}`
-        }))
+        })
+        markers.push(newMarker)
+        map.addOverlay(newMarker)
       })
     })
+
+    addressInput.addEventListener('keydown', ({key, target}) => {
+    if (key == 'Enter') {
+      const address = target.value.replace(/\s/g, '+')
+      markers.forEach(marker => map.removeOverlay(marker))
+      fetch(`https://api.geocod.io/v1.4/geocode?api_key=596e1857bc5e3d3ad58c153b0e55d0abca91890&fields=&q=${address},+Philadelphia,+PA`)
+        .then(response => response.json())
+        .then(({results}) => {
+         markers.forEach(marker => map.removeOverlay(marker))
+          const location = results[0].location
+          map.removeOverlay(userMarker)
+          userMarker.setPosition(fromLonLat([location.lng, location.lat]))
+          map.addOverlay(userMarker)
+          map.values_.view.animate({
+            center: fromLonLat([location.lng, location.lat]),
+            zoom: 18
+          })
+          // Ordering by closest to user input:
+          const orderedStations = stations.sort((a, b) => {
+            return haversine({ lat: location.lat, lon: location.lng }, { lat: a.geometry.coordinates[1], lon: a.geometry.coordinates[0]}) > 
+                   haversine({ lat: location.lat, lon: location.lng }, { lat: b.geometry.coordinates[1], lon: b.geometry.coordinates[0]})
+          })
+          sidebar.innerHTML = ''
+          orderedStations.slice(0, 5).forEach(kiosk => {
+            const station = new Station(kiosk)
+            sidebar.appendChild(renderer.createStationInfoDiv(station, {
+              div: ['px-3', 'py-4', 'border-b', 'border-gray-300', 'flex', 'flex-col', 'hover:bg-gray-100', 'hover:cursor-pointer'],
+              title: ['text-xl', 'font-bold'],
+              paragraph: ['text-base']
+            }))
+            const newMarker = new Overlay({
+              position: fromLonLat([station.coordinates.longitude, station.coordinates.latitude]),
+              positioning: 'top-left',
+              element: document.createElement('div'),
+              stopEvent: false,
+              className: `marker ${station.status}`
+            })
+            markers.push(newMarker)
+            map.addOverlay(newMarker)
+          })
+        })    
+    }
+  })
 })
 
 
